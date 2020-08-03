4afe674e54a1df7a86d3094d664cc2d1928f3662
Kenny Levinsen <kl@kl.wtf>
2020-08-03 02:11:29 +0200

    linked_list: Initial implementation

---
+++ common/linked_list.c
@@ -0,0 +1,33 @@
+#include <assert.h>
+#include <stdbool.h>
+#include <stddef.h>
+
+#include "linked_list.h"
+
+void linked_list_init(struct linked_list *list) {
+	list->next = list;
+	list->prev = list;
+}
+
+void linked_list_insert(struct linked_list *list, struct linked_list *elem) {
+	assert(list->prev != NULL && list->next != NULL);
+	assert(elem->prev == NULL && elem->next == NULL);
+
+	elem->prev = list;
+	elem->next = list->next;
+	list->next = elem;
+	elem->next->prev = elem;
+}
+
+void linked_list_remove(struct linked_list *elem) {
+	assert(elem->prev != NULL && elem->next != NULL);
+
+	elem->prev->next = elem->next;
+	elem->next->prev = elem->prev;
+	elem->next = NULL;
+	elem->prev = NULL;
+}
+
+bool linked_list_empty(struct linked_list *list) {
+	return list->next == list;
+}
