704d1ccd0b9e0da1f3555a07ba9184927983f812
Kenny Levinsen <kl@kl.wtf>
2020-08-31 22:00:15 +0200

    server: Remember to remove seat from list

--- seatd/server.c
+++ seatd/server.c
@@ -53,6 +53,7 @@ void server_finish(struct server *server) {
 	assert(server);
 	while (!linked_list_empty(&server->seats)) {
 		struct seat *seat = (struct seat *)server->seats.next;
+		linked_list_remove(&seat->link);
 		seat_destroy(seat);
 	}
 	poller_finish(&server->poller);
