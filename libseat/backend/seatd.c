kennylevinsen/seatd

commit 166feaea3394e00af14418e074ae090e31922f33

Make libseat_seat_listener const
libseat will never write to that struct. Let's allow callers to
make it read-only.

libseat/backend/seatd.c
@@ -33,7 +33,7 @@ struct pending_event {
 struct backend_seatd {
 	struct libseat base;
 	struct connection connection;
-	struct libseat_seat_listener *seat_listener;
+	const struct libseat_seat_listener *seat_listener;
 	void *seat_listener_data;
 	struct linked_list pending_events;
 	bool error;
@@ -363,7 +363,7 @@ static int dispatch_and_execute(struct libseat *base, int timeout) {
 	return predispatch + postdispatch;
 }
 
-static struct libseat *_open_seat(struct libseat_seat_listener *listener, void *data, int fd) {
+static struct libseat *_open_seat(const struct libseat_seat_listener *listener, void *data, int fd) {
 	assert(listener != NULL);
 	assert(listener->enable_seat != NULL && listener->disable_seat != NULL);
 	struct backend_seatd *backend = calloc(1, sizeof(struct backend_seatd));
@@ -411,7 +411,7 @@ alloc_error:
 	return NULL;
 }
 
-static struct libseat *open_seat(struct libseat_seat_listener *listener, void *data) {
+static struct libseat *open_seat(const struct libseat_seat_listener *listener, void *data) {
 	int fd = seatd_connect();
 	if (fd == -1) {
 		return NULL;

include/backend.h
@@ -16,7 +16,7 @@ struct named_backend {
 };
 
 struct seat_impl {
-	struct libseat *(*open_seat)(struct libseat_seat_listener *listener, void *data);
+	struct libseat *(*open_seat)(const struct libseat_seat_listener *listener, void *data);
 	int (*disable_seat)(struct libseat *seat);
 	int (*close_seat)(struct libseat *seat);
 	const char *(*seat_name)(struct libseat *seat);

include/libseat.h
@@ -55,7 +55,7 @@ struct libseat_seat_listener {
  * Returns a pointer to an opaque libseat struct on success. Returns NULL and
  * sets errno on error.
  */
-struct libseat *libseat_open_seat(struct libseat_seat_listener *listener, void *userdata);
+struct libseat *libseat_open_seat(const struct libseat_seat_listener *listener, void *userdata);
 
 /*
  * Disables a seat, used in response to a disable_seat event. After disabling

libseat/backend/logind.c
@@ -30,7 +30,7 @@
 
 struct backend_logind {
 	struct libseat base;
-	struct libseat_seat_listener *seat_listener;
+	const struct libseat_seat_listener *seat_listener;
 	void *seat_listener_data;
 
 	sd_bus *bus;
@@ -621,7 +621,7 @@ static int set_type(struct backend_logind *backend, const char *type) {
 	return ret;
 }
 
-static struct libseat *logind_open_seat(struct libseat_seat_listener *listener, void *data) {
+static struct libseat *logind_open_seat(const struct libseat_seat_listener *listener, void *data) {
 	struct backend_logind *backend = calloc(1, sizeof(struct backend_logind));
 	if (backend == NULL) {
 		return NULL;

libseat/backend/noop.c
@@ -13,7 +13,7 @@
 
 struct backend_noop {
 	struct libseat base;
-	struct libseat_seat_listener *seat_listener;
+	const struct libseat_seat_listener *seat_listener;
 	void *seat_listener_data;
 
 	bool initial_setup;
@@ -103,7 +103,7 @@ static int dispatch_background(struct libseat *base, int timeout) {
 	return 0;
 }
 
-static struct libseat *noop_open_seat(struct libseat_seat_listener *listener, void *data) {
+static struct libseat *noop_open_seat(const struct libseat_seat_listener *listener, void *data) {
 	struct backend_noop *backend = calloc(1, sizeof(struct backend_noop));
 	if (backend == NULL) {
 		return NULL;

libseat/libseat.c
@@ -34,7 +34,7 @@ static const struct named_backend impls[] = {
 #error At least one backend must be enabled
 #endif
 
-struct libseat *libseat_open_seat(struct libseat_seat_listener *listener, void *data) {
+struct libseat *libseat_open_seat(const struct libseat_seat_listener *listener, void *data) {
 	if (listener == NULL || listener->enable_seat == NULL || listener->disable_seat == NULL) {
 		errno = EINVAL;
 		return NULL;

commit 038c30f9b1f93fb7fc9514366db3f3aa61a3ca49

libseat: Fix build of builtin backend
This was regressed by 166feaea3394e00af14418e074ae090e31922f33 which
missed the builtin backend when changing struct libseat_seat_listener to
being passed around as const.

libseat/backend/seatd.c
@@ -599,7 +599,7 @@ static int set_deathsig(int signal) {
 #error Unsupported platform
 #endif
 
-static struct libseat *builtin_open_seat(struct libseat_seat_listener *listener, void *data) {
+static struct libseat *builtin_open_seat(const struct libseat_seat_listener *listener, void *data) {
 	int fds[2];
 	if (socketpair(AF_UNIX, SOCK_STREAM, 0, fds) == -1) {
 		log_errorf("Could not create socket pair: %s", strerror(errno));