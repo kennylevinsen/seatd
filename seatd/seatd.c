ae42d05513189c2215916c5586e9f8fb74c4d5e7
Kenny Levinsen <kl@kl.wtf>
2022-02-26 22:56:42 +0100

    seatd: Change default log-level to info

--- seatd/seatd.c
+++ seatd/seatd.c
@@ -72,7 +72,7 @@ int main(int argc, char *argv[]) {
 	int readiness = -1;
 	bool unlink_existing_socket = true;
 	bool chown_socket = true;
-	enum libseat_log_level level = LIBSEAT_LOG_LEVEL_ERROR;
+	enum libseat_log_level level = LIBSEAT_LOG_LEVEL_INFO;
 	while ((c = getopt(argc, argv, "vhn:g:u:l:z")) != -1) {
 		switch (c) {
 		case 'n':
